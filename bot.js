c7106b36b6df3eade91dab1dbcba0cdfbf673561
Yifan He <yifanhe13@gmail.com>
2020-06-23 00:52:54 -0700

    make it better

---
+++ bot.js
@@ -0,0 +1,109 @@
+//  __   __  ___        ___
+// |__) /  \  |  |__/ |  |  
+// |__) \__/  |  |  \ |  |  
+
+// This is the main file for the Optimus_Prime bot.
+
+// Import Botkit's core features
+const { Botkit } = require('botkit');
+const { BotkitCMSHelper } = require('botkit-plugin-cms');
+
+// Import a platform-specific adapter for web.
+
+const { WebAdapter } = require('botbuilder-adapter-web');
+
+const { MongoDbStorage } = require('botbuilder-storage-mongodb');
+
+// Load process.env values from .env file
+require('dotenv').config();
+
+let storage = null;
+if (process.env.MONGO_URI) {
+    storage = mongoStorage = new MongoDbStorage({
+        url : process.env.MONGO_URI,
+    });
+}
+
+
+const adapter = new WebAdapter({});
+
+
+const controller = new Botkit({
+    webhook_uri: '/api/messages',
+
+    studio_command_uri: process.env.studio_command_uri,
+    studio_token: process.env.studio_token,
+    
+    adapter: adapter,
+
+    storage
+});
+
+if (process.env.CMS_URI) {
+    controller.usePlugin(new BotkitCMSHelper({
+        uri: process.env.CMS_URI,
+        token: process.env.CMS_TOKEN,
+    }));
+}
+
+// Once the bot has booted up its internal services, you can use them to do stuff.
+controller.ready(() => {
+
+    // load traditional developer-created local custom feature modules
+    controller.loadModules(__dirname + '/features');
+
+    /* catch-all that uses the CMS to trigger dialogs */
+    if (controller.plugins.cms) {
+        controller.on('message,direct_message', async (bot, message) => {
+            let results = false;
+            results = await controller.plugins.cms.testTrigger(bot, message);
+
+            if (results !== false) {
+                // do not continue middleware!
+                return false;
+            }
+        });
+    }
+
+});
+
+// This captures and evaluates any message sent to the bot as a DM
+// or sent to the bot in the form "@bot message" and passes it to
+// Botkit Studio to evaluate for trigger words and patterns.
+// If a trigger is matched, the conversation will automatically fire!
+// You can tie into the execution of the script using the functions
+// controller.studio.before, controller.studio.after and controller.studio.validate
+if (process.env.studio_token) {
+    controller.on('message_received', function(bot, message) {
+        controller.studio.runTrigger(bot, message.text, message.user, message.channel, message).then(function(convo) {
+            if (!convo) {
+              // web bot requires a response of some kind!
+              bot.reply(message,'OK');
+
+                // no trigger was matched
+                // If you want your bot to respond to every message,
+                // define a 'fallback' script in Botkit Studio
+                // and uncomment the line below.
+                // controller.studio.run(bot, 'fallback', message.user, message.channel, message);
+            } else {
+                // set variables here that are needed for EVERY script
+                // use controller.studio.before('script') to set variables specific to a script
+                convo.setVar('current_time', new Date());
+                convo.setVar('bot', controller.studio_identity);
+            }
+        }).catch(function(err) {
+            bot.reply(message, 'I experienced an error with a request to Botkit Studio: ' + err);
+            debug('Botkit Studio: ', err);
+        });
+    });
+} else {
+
+    console.log('~~~~~~~~~~');
+    console.log('NOTE: Botkit Studio functionality has not been enabled');
+    console.log('To enable, pass in a studio_token parameter with a token from https://studio.botkit.ai/');
+}
+
+
+
+
+
